462620c0041a254a890c947335ffb02c99da6ef2
hoanghuy <dinhhoanghuy20044@gmail.com>
2024-10-05 20:50:28 +0700

    Thêm các thay đổi mới vào nhánh updatehompage

--- client/src/components/HomePage/PopularDestinations.jsx
+++ client/src/components/HomePage/PopularDestinations.jsx
@@ -1,35 +1,46 @@
 // PopularDestinations.jsx
 import React from "react";
 import { Card, Button, Col, Row } from "antd";
-import 'antd/dist/antd';
-
+import "antd/dist/antd";
 
 const { Meta } = Card;
 
 const PopularDestinations = () => {
   const destinations = [
-    { name: "Istanbul, Turkey", image: "image.png" },
-    { name: "Sydney, Australia", image: "image.png" },
-    { name: "Baku, Azerbaijan", image: "image.png" },
+    { name: "TP Hồ Chí Minh", image: "image.png" },
+    { name: "TP Hà Nội", image: "image.png" },
+    { name: "TP Cần Thơ", image: "image.png" },
   ];
 
   return (
     <section className="popular-destinations">
-      <h2>Plan your perfect trip</h2>
-      <p>Search Flights & Places Hire to our most popular destinations</p>
+      <div className="header">
+        <h2>Tìm kiếm địa điểm du lịch</h2>
+        <Button type="primary" className="see-more-btn">
+          Xem thêm
+        </Button>
+      </div>
       <Row gutter={16}>
         {destinations.map((destination, index) => (
           <Col span={8} key={index}>
             <Card
               hoverable
-              cover={<img alt={destination.name} src={destination.image} style={{ height: 150, objectFit: 'cover' }} />}
+              cover={
+                <img
+                  alt={destination.name}
+                  src={destination.image}
+                  style={{ height: 150, objectFit: "cover" }}
+                />
+              }
             >
-              <Meta title={destination.name} description="Flights • Hotels • Resorts" />
+              <Meta
+                title={destination.name}
+                description="Khách sạn • Khu nghỉ dưỡng"
+              />
             </Card>
           </Col>
         ))}
       </Row>
-      <Button type="primary" className="see-more-btn">See more places</Button>
     </section>
   );
 };
