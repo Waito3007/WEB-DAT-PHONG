1e86167da1f64a7691dd5e8b762c434c6d38dab8
San <sangvu2015dp1@gmai.com>
2024-10-13 23:37:08 +0700

    update: ẩn url mongo

--- config/db.js
+++ config/db.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
+require('dotenv').config(); // Đọc biến môi trường từ file .env
 
-// Chuỗi kết nối MongoDB Atlas
-const mongoURI = 'mongodb+srv://sangvu:sangvu3007HSGamer@bookingweb.pdloq.mongodb.net/?retryWrites=true&w=majority&appName=BookingWeb';
-// Hàm để kết nối đến MongoDB
+const mongoURI = process.env.MONGO_URI; // Lấy URI từ file .env
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(mongoURI);
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('Failed to connect to MongoDB', err);
     process.exit(1); // Dừng ứng dụng nếu không kết nối được
   }
 };
+
 module.exports = connectDB;
