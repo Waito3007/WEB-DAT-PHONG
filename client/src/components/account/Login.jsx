f17a455b8d230e10d3089d10a44725ec86002dee
San <sangvu2015dp1@gmai.com>
2024-10-14 02:10:06 +0700

    Update:Login

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -1,122 +1,86 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Form, Input, Button, message } from 'antd';
-import '../../App.css';
-import { Content } from 'antd/es/layout/layout';
+import { Input } from 'antd';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
-  const handleLogin = async (values) => {
-    setError(null); // Reset lỗi trước khi gửi yêu cầu
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError(null);
 
     try {
       const response = await fetch('/api/users/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email: values.email, password: values.password }),
+        body: JSON.stringify({ email, password }),
       });
 
       const data = await response.json();
       if (response.ok) {
-        localStorage.setItem('token', data.token); // Store token
-        localStorage.setItem('user', JSON.stringify(data.user)); // Store user info
-        message.success('Đăng nhập thành công'); // Sử dụng Ant Design message
-        navigate('/addhotel'); // Redirect to profile page
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('user', JSON.stringify(data.user));
+        alert('Đăng nhập thành công');
+        navigate('/');
       } else {
         setError(data.msg || 'Có lỗi xảy ra');
-        message.error(data.msg || 'Có lỗi xảy ra'); // Sử dụng Ant Design message
       }
     } catch (err) {
       console.error('Lỗi mạng hoặc server:', err);
       setError('Có lỗi xảy ra, vui lòng thử lại sau.');
-      message.error('Có lỗi xảy ra, vui lòng thử lại sau.'); // Sử dụng Ant Design message
     }
   };
 
   return (
-    <div className="w-[1440px] h-[1024px] pl-[89px] pr-[102px] py-[104px] bg-white justify-start items-start gap-[119px] inline-flex">
-      <div className="w-[512px] self-stretch flex-col justify-start items-start gap-12 inline-flex">
-        <div className="self-stretch h-[49px] flex-col justify-start items-start gap-4 flex">
-          <div className="text-center text-black text-[40px] font-normal font-['Alexandria']">
-            Đăng nhập tài khoản của bạn
-          </div>
-        </div>
-        <Form
-          name="login"
-          onFinish={handleLogin}
-          layout="vertical"
-          className="self-stretch h-[451px] flex-col justify-start items-start gap-10 flex"
-        >
-          <div className="self-stretch h-[177px] flex-col justify-start items-start gap-6 flex">
-            <Form.Item
-              label="Email"
-              name="email"
-              rules={[{ required: true, message: 'Vui lòng nhập email!' }]}
-            >
-              <Input
-                type="email"
-                style={{
-                  borderColor: 'black',
-                }}
-                className="w-[512px] h-14 rounded-tl rounded-tr"
-                required
-              />
-            </Form.Item>
-
-            <Form.Item
-              label="Mật khẩu"
-              name="password"
-              rules={[{ required: true, message: 'Vui lòng nhập mật khẩu!' }]}
-            >
-              <Input.Password
-                style={{
-                  borderColor: 'black',
-                }}
-                className="w-[512px] h-14 rounded-tl rounded-tr"
-                required
-              />
-            </Form.Item>
-          </div>
-
-          {error && <p className="text-red-500 text-sm">{error}</p>}
-
-          <div className="self-stretch justify-start items-center gap-[252px] inline-flex">
-            <div className="w-[512px] text-right text-[#ff8682] text-sm font-medium font-['Montserrat']">
-              Quên mật khẩu
-            </div>
-          </div>
-
-          <div className="flex-col justify-start items-start gap-4 flex">
-            <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-                className="self-stretch h-12 px-4 py-2 bg-black rounded justify-center items-center gap-1"
-              >
-                Đăng nhập
-              </Button>
-            </Form.Item>
-          </div>
-        </Form>
-
-        <div className="self-stretch text-center">
-        <span style={{ color: '#112211', fontSize: '12px', fontWeight: '500', fontFamily: 'Montserrat' }}>
-          Bạn chưa có tài khoản?
-        </span>
-        <span style={{ color: '#ff8682', fontSize: '12px', fontWeight: '600', fontFamily: 'Montserrat' }}>
-          Đăng ký
-        </span>
-        </div>
+    <div className="flex flex-col md:flex-row justify-center items-start px-4 py-8 md:px-16 md:py-16 bg-white">
+      <div className="w-[488px] h-[816px] relative mb-8 md:mb-0">
+        <img
+          className="w-[100%] h-auto rounded-lg"
+          src="https://res.cloudinary.com/dackig67m/image/upload/v1728844619/image_2024-10-14_013655362_w9eudg.png"
+          alt="Login" />
       </div>
-      <div className="w-[618px] h-[816px] relative">
-        <img className="w-[616px] h-[816px] left-0 top-0 absolute rounded-[30px]" src="https://res.cloudinary.com/dackig67m/image/upload/v1728840647/register_srtwnf.png" alt="Login Image" />
-        <div className="w-[616px] h-[58px] left-[2px] top-[816px] absolute bg-gradient-to-b from-black to-black rounded-tl-[30px] rounded-tr-[30px]" />
-        <div className="h-2.5 left-[274px] top-[782px] absolute justify-start items-start gap-2 inline-flex">
-          <div className="w-8 h-2.5 bg-[#8dd3bb] rounded-[5px]" />
-          <div className="w-2.5 h-2.5 bg-white rounded-full" />
-          <div className="w-2.5 h-2.5 bg-white rounded-full" />
+      <div className="w-full md:w-1/2 flex flex-col justify-start items-start gap-6 ml-11">
+        <h2 className="text-black text-4xl font-normal text-center md:text-left">Đăng Nhập <br /> Tài Khoản Của Bạn</h2>
+        {error && <div className="text-red-500 mb-4 text-center">{error}</div>}
+        <form onSubmit={handleLogin} className="flex flex-col gap-6">
+          <div className="flex flex-col">
+            <label className="text-[#1c1b1f] text-sm">Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full px-4 input-field h-12" // Chiều rộng 100% cho ô nhập
+              style={{ color: 'black', width: '100%' }} // Thay đổi màu chữ thành đen và chiều rộng 100%
+              required
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label className="text-[#1c1b1f] text-sm">Mật khẩu</label>
+            <Input.Password
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full input-field h-12" // Chiều rộng 100% cho ô nhập
+              style={{ width: '100%' }} // Đảm bảo chiều rộng là 100%
+              required
+            />
+            <a href="/forgotpassword" className="text-red-500 text-sm text-right mt-1">Quên mật khẩu?</a>
+          </div>
+
+          <div className="flex items-center justify-center">
+            <button type="submit" className="w-full bg-black text-white py-2 rounded-md text-lg h-12"> 
+              Đăng Nhập
+            </button>
+          </div>
+        </form>
+        <div className="flex justify-center">
+          <p className="text-sm text-black">
+            Bạn chưa có tài khoản? 
+            <a href="/register" className="text-red-500 ml-1">Đăng ký</a>
+          </p>
         </div>
       </div>
     </div>
